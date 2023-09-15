0c65e234250393876c0565c9d62d50b5f2221597
Dennis Lee <dl@aduna.ch>
2023-09-15 15:34:10 +0200

    testing pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// export default defineConfig({
+// 	plugins: [sveltekit()]
+// });
+
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			  }
+		}),
+	]
+})
