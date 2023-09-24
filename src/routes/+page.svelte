a3467369fef0dc58dd57fec45dabddec0ae95e1b
JolloDede <jollodede@gmail.com>
2023-09-24 18:28:00 +0200

    feat: calendar is displayed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,41 @@
 <script lang="ts">
 	import CalendarDay from '../components/CalendarDay.svelte';
-import Button from '../components/default/Button.svelte';
+	import Button from '../components/default/Button.svelte';
 	import FancyButton from '../components/default/FancyButton.svelte';
 	import Main from '../components/default/Main.svelte';
 	import PrimaryButton from '../components/default/PrimaryButton.svelte';
 	import SecondaryButton from '../components/default/SecondaryButton.svelte';
-	let daysInMonth = new Date(2023, 9, 0).getDate();
+
+	let dateToday = new Date();
+	let firstDay = new Date(dateToday.getFullYear(), dateToday.getMonth(), 1);
+	let indexOfFirstDay = firstDay.getDay();
+	if (indexOfFirstDay == 0) indexOfFirstDay = 7;
+	let lastDay = new Date(dateToday.getFullYear(), dateToday.getMonth() + 1, 0);
+
+	let dayArr: number[] = [];
+	for (let i = 1; i < 43; i++) {
+		if (i >= indexOfFirstDay - 1) {
+			if (i - indexOfFirstDay >= lastDay.getDate()) {
+				dayArr.push(0);
+			} else {
+				dayArr.push(i - indexOfFirstDay + 1);
+			}
+		} else {
+			dayArr.push(0);
+		}
+	}
 </script>
 
 <Main>
 	<div class="m-4 p-4 bg-sapphire-50 border-sapphire-400 rounded-lg">
 		<a href="today" class="text-sapphire-600"> today you are gona change the world! </a>
 	</div>
-	<div class="flex flex-col m-4 p-4 bg-sapphire-50 text-zinc-800 rounded-lg" style="height: 28rem; width: 24rem;">
+	<div
+		class="flex flex-col m-4 p-4 bg-sapphire-50 text-zinc-800 rounded-lg"
+		style="height: 28rem; width: 24rem;"
+	>
 		<div class="font-semibold text-2xl">Calendar</div>
-		<div class="flex flex-wrap">
+		<div class="grid grid-cols-7">
 			<CalendarDay>Mo</CalendarDay>
 			<CalendarDay>Di</CalendarDay>
 			<CalendarDay>Mi</CalendarDay>
@@ -22,9 +43,11 @@ import Button from '../components/default/Button.svelte';
 			<CalendarDay>Fr</CalendarDay>
 			<CalendarDay>Sa</CalendarDay>
 			<CalendarDay>So</CalendarDay>
-			{#each {length: daysInMonth} as _, day}
-				<div >
-					{day}
+			{#each dayArr as day}
+				<div>
+					{#if day != 0}
+						{day}
+					{/if}
 				</div>
 			{/each}
 		</div>
