97b0263154612ff4c54125c215eecf9d58b5ef70
JolloDede <jollodede@gmail.com>
2023-09-24 14:47:04 +0200

    feat: Exercise sets can be finished buttons turn green when clicked and red when clicked again

--- src/components/Icon/Circle.svelte
+++ src/components/Icon/Circle.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	let clazz = "";
-	export { clazz as class};
+	let clazz = '';
+	export { clazz as class };
 </script>
 
-<svg class={"w-10"+" "+clazz} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+<svg class={'w-10 ' + clazz} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
 	><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
 		id="SVGRepo_tracerCarrier"
 		stroke-linecap="round"
@@ -11,7 +11,6 @@
 	/><g id="SVGRepo_iconCarrier">
 		<path
 			d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-			stroke="#000000"
 			stroke-width="2"
 			stroke-linecap="round"
 			stroke-linejoin="round"
---
+++ src/components/SetButton.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { SetState } from '../types';
+	import Circle from './Icon/Circle.svelte';
+	import Button from './default/Button.svelte';
+
+	let state = SetState.Default;
+
+	function handleClick() {
+		switch (state) {
+			case SetState.Default:
+				state = SetState.Done;
+				break;
+			case SetState.Done:
+				state = SetState.Failed;
+				break;
+			case SetState.Failed:
+				state = SetState.Default;
+				break;
+
+			default:
+				console.log('Error the state is not defined!');
+				break;
+		}
+	}
+</script>
+
+<Button on:click={handleClick}>
+	{#if state == SetState.Default}
+		<Circle class="stroke-black" />
+	{/if}
+	{#if state == SetState.Done}
+		<Circle class="fill-green-600 stroke-green-600" />
+	{/if}
+	{#if state == SetState.Failed}
+		<Circle class="fill-red-600 stroke-red-600" />
+	{/if}
+</Button>
